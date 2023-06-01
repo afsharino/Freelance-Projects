e54be81b7f673b67cc1127863074a7ad82e3bdbc
afsharino-archiso <mohammadafshari.fum.ac.ir@gmail.com>
2023-06-01 14:49:41 +0330

    make changes to scraper to not overwrite images and update main

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,6 @@
 from Scraper import get_captcha
-from tqdm.notebook import tqdm 
+
 
 if __name__ == '__main__':
-    for i in tqdm(range(3000)):
+    for i in range(3000):
         get_captcha(i)
--- src/Scraper.py
+++ src/Scraper.py
@@ -20,20 +20,22 @@ def get_captcha(index):
         
             # Step 4: Download the captcha Image
             captcha_image = requests.get(captcha_url)
-            file_name = f'captcha_{index}.png'
-            file_path = os.path.join('./downloaded_images', file_name)
+            items = os.listdir(f'{os.getcwd()}/downloaded_images')
+
+            if index < len(items):
+                file_name = f'captcha_{len(items)}.png'
+                file_path = os.path.join('./downloaded_images', file_name)
+            else:
+                file_name = f'captcha_{index}.png'
+                file_path = os.path.join('./downloaded_images', file_name)
 
             with open(file_path , 'wb') as file:
                 file.write(captcha_image.content)
-                f'Captcha image saved as {file_name}'
+                print(f'Captcha image saved as {file_name}')
             
     except Exception as e:
         print(f"error raised! the status code is {response.status_code}.")
         print(f'eror: {e}')
-    
-
-
-
 
 
 
