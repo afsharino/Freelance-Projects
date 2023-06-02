d442ccdbf8030a447ee3cb99471df0c2ad5423b7
afsharino-archiso <mohammadafshari.fum.ac.ir@gmail.com>
2023-06-02 18:13:44 +0330

    remove main.py and annotator.py module and complete annotator.ipynb

--- src/Main.py
+++
@@ -1,6 +0,0 @@
-from Scraper import get_captcha
-import concurrent.futures
-
-if __name__ == '__main__':
-   with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
-     executor.map(get_captcha, range(2))
