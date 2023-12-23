6d9420d15f74c2d74745e2299d325143c78d63b5
afsharino@archiso <mohammadafshari.fum.ac.ir@gmial.com>
2023-12-23 15:08:03 +0330

    complete .py file

--- Movie-Regression/src/final.py
+++ Movie-Regression/src/final.py
@@ -12,11 +12,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsRegressor
 
 # Deep Learning
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.neural_network import MLPRegressor
 
 # Others
 import warnings
@@ -35,8 +37,10 @@ data.drop_duplicates(inplace=True)
 X = data[['budget', 'sequel', 'screens']]
 y = data.gross
 
+# Train test Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Scale data
 scaler_X = StandardScaler()
 X_train_scaled = scaler_X.fit_transform(X_train)
 X_test_scaled = scaler_X.transform(X_test)
@@ -45,6 +49,7 @@ scaler_y = StandardScaler()
 y_train_scaled = scaler_y.fit_transform(y_train.values.reshape(-1, 1))
 y_test_scaled = scaler_y.transform(y_test.values.reshape(-1, 1))
 
+# Fit Decision Tree model
 dt_model = DecisionTreeRegressor(max_depth=2)
 _ = dt_model.fit(X_train_scaled, y_train_scaled)
 
@@ -58,7 +63,10 @@ print(f"The rmse of decission tree is: {dt_mse}")
 
 dt_mae = mean_absolute_error(y_test_scaled, y_pred_dt)
 print(f"The mae of decission tree is: {dt_mae}")
+sns.heatmap(np.corrcoef(y_pred_dt,y_test_scaled.flatten()), cmap='YlGnBu', annot=True, linewidths = 0.2);
+plt.show()
 
+# Fit Neural Network
 nn_model = Sequential()
 nn_model.add((Dense(128, input_dim=X_train_scaled.shape[1], activation='relu')))
 nn_model.add((Dense(64, activation='relu')))
@@ -93,3 +101,37 @@ print(f"The rmse of neural net is: {nn_rmse}")
 
 nn_mae = mean_absolute_error(y_test_scaled, y_pred_nn)
 print(f"The mae of neural net is: {nn_mae}")
+sns.heatmap(np.corrcoef(y_pred_nn.flatten(),y_test_scaled.flatten()), cmap='YlGnBu', annot=True, linewidths = 0.2);
+plt.show()
+# Fit K nearest Neighbors
+# Find optimal k
+rmse_val = []
+for K in range(20):
+    K = K+1
+    model = KNeighborsRegressor(n_neighbors = K, n_jobs=-1)
+
+    model.fit(X_train_scaled, y_train_scaled)  #fit the model
+    pred=model.predict(X_test_scaled) #make prediction on test set
+    error = np.sqrt(mean_squared_error(y_test_scaled,pred)) #calculate rmse
+    rmse_val.append(error) #store rmse values
+    print('RMSE value for k= ' , K , 'is:', error)
+
+plt.plot(list(range(1,21,1)), rmse_val)
+plt.show()
+
+knn_model = KNeighborsRegressor(n_neighbors = 3, n_jobs=-1)
+_ = knn_model.fit(X_train_scaled, y_train_scaled)
+
+y_pred_knn = knn_model.predict(X_test_scaled)
+
+knn_mse = mean_squared_error(y_test_scaled, y_pred_knn)
+print(f"The mse of k nearest neighbors is: {knn_mse}")
+
+knn_rmse = np.sqrt(mean_squared_error(y_test_scaled, y_pred_knn))
+print(f"The rmse of k nearest neighbors is: {knn_rmse}")
+
+knn_mae = mean_absolute_error(y_test_scaled, y_pred_knn)
+print(f"The mae of k nearest neighbors is: {knn_mae}")
+
+sns.heatmap(np.corrcoef(y_pred_knn.flatten(),y_test_scaled.flatten()), cmap='YlGnBu', annot=True, linewidths = 0.2);
+plt.show()
